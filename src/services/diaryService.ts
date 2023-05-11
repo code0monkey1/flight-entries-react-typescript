6e67f6848b64b53753a96d2bb1ddf67507a48be8
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-11 12:37:27 +0000

    11 May 2023, 18:07

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry } from '../types';
 
-const baseUrl = 'https://localhost:3000/api/'
+const baseUrl = 'http://localhost:3001/api/'
 
 export const getDiaries = () =>
-  axios.get<NonSensitiveDiaryEntry[]>(baseUrl).then(response => response.data)
+  axios.get<NonSensitiveDiaryEntry[]>(baseUrl+"diaries").then(response => response.data)
 
 export const createDiary = (object: NewDiaryEntry) =>
-  axios.post<DiaryEntry[]>(baseUrl, object).then(response => response.data)
+  axios.post<DiaryEntry[]>(baseUrl+"diaries", object).then(response => response.data)
